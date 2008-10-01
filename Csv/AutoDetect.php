4fc127f781f76d222da005e679f826489a7d4d4c
luke.visinoni <luke.visinoni@0946aa64-6b46-0410-9bca-13cabbcf55d9>
2008-10-01 17:34:20 +0000

    Integrated Edward's changes into Csv_AutoDetect (formerly Csv_Sniffer)
    git-svn-id: http://php-csv-utils.googlecode.com/svn/trunk@44 0946aa64-6b46-0410-9bca-13cabbcf55d9

--- Csv/AutoDetect.php
+++ Csv/AutoDetect.php
@@ -1,6 +1,9 @@
 <?php
+require_once 'Exception/CannotDetermineDialect.php';
+require_once 'Exception/DataSampleTooShort.php';
+require_once 'Reader/String.php';
 /**
- * CSV Utils - AutoDetect
+ * CSV Utils - detecter
  * 
  * This class accepts a sample of csv and attempts to deduce its format. It then
  * can return a Csv_Dialect tailored to that particular csv file
@@ -11,9 +14,6 @@
  * @license GNU Lesser General Public License
  * @version 0.1
  */
-require_once 'Csv/Exception/CannotDetermineDialect.php';
-require_once 'Csv/Exception/DataSampleTooShort.php';
-require_once 'Csv/Reader/String.php';
 /**
  * Attempts to deduce the format of a csv file
  * 
@@ -27,24 +27,43 @@ class Csv_AutoDetect
      * returns the basic csv dialect
      * 
      * @param string A piece of sample data used to deduce the format of the csv file
-     * @return array An array with the first value being the quote char and the second the delim
+     * @return Csv_Dialect A {@link Csv_Dialect} object with the appropriate settings
      * @access protected
      */
     public function detect($data) {
-    
+
+    	if (strlen($data)==0) {
+            throw new Csv_Exception_DataSampleTooShort('You must provide at least ten lines in your sample data');/*
+    		// this is an empty file - provide some sane defaults
+	        $dialect = new Csv_Dialect();
+	        $dialect->delimiter = ',';
+	        $dialect->quotechar = '"';
+	        $dialect->quoting = Csv_Dialect::QUOTE_MINIMAL;
+	        $dialect->lineterminator = "\r\n";
+	        return $dialect;*/
+    	}
+        
+    	// this is a non-empty file
+    	$linefeed = $this->guessLinefeed($data);
+    	
         list($quote, $delim) = $this->guessQuoteAndDelim($data);
+        
+        if (!$quote) {
+        	$quote = '"';
+        }
+        
         if (is_null($delim)) {
-            if (!$delim = $this->guessDelim($data)) {
+            if (!$delim = $this->guessDelim($data, $linefeed, $quote)) {
                 throw new Csv_Exception_CannotDetermineDialect('Csv_AutoDetect was unable to determine the file\'s dialect.');
             }
         }
+        
         $dialect = new Csv_Dialect();
-        if (!$quote) {
-            // @todo: figure out if this is the best way to go about this
-            $dialect->quotechar = '"';
-            $dialect->quoting = Csv_Dialect::QUOTE_NONE;
-        }
+        $dialect->quotechar = $quote;
+        $dialect->quoting = $this->guessQuotingStyle($data, $quote, $delim, $linefeed);
         $dialect->delimiter = $delim;
+        $dialect->lineterminator = $linefeed;
+        
         return $dialect;
     
     }
@@ -59,6 +78,12 @@ class Csv_AutoDetect
     
         $reader = new Csv_Reader_String($data, $this->detect($data));
         list($has_headers, $checked, $types, $lengths, $total_lines, $headers) = array(0, 0, array(), array(), $reader->count(), $reader->getRow());
+        
+        if ($total_lines<=2) {
+        	// please try again with a a larger file :)
+        	return false;
+        }
+        
         $total_columns = count($headers);
         foreach (range(0, $total_columns-1) as $key => $col) $types[$col] = null;
         // loop through each remaining rows
@@ -163,45 +188,199 @@ class Csv_AutoDetect
      * @access protected
      * @return mixed If a delimiter can be found it is returned otherwise false is returned
      */
-    protected function guessDelim($data) {
+    protected function guessDelim($data, $linefeed, $quotechar) {
     
+        $count = count(explode($linefeed, $data));
+        if ($count < 10) throw new Csv_Exception_DataSampleTooShort('You must provide at least ten lines in your sample data');
+	    $charcount = count_chars($data, 1);
+	    
+	    $filtered = array();
+	    foreach ($charcount as $char=>$count) {
+	    	if ($char==ord($quotechar)) {
+	    		// exclude the quote char
+	    		continue;
+	    	}
+	    	if ($char==ord(" ")) {
+	    		// exclude spaces
+	    		continue;
+	    	}
+	    	if ($char>=ord("a") && $char<=ord("z")) {
+	    		// exclude a-z
+	    		continue;
+	    	}
+	    	if ($char>=ord("A") && $char<=ord("Z")) {
+	    		// exclude A-Z
+	    		continue;
+	    	}
+	    	if ($char>=ord("0") && $char<=ord("9")) {
+	    		// exclude 0-9
+	    		continue;
+	    	}
+	    	if ($char==ord("\n") || $char==ord("\r")) {
+	    		// exclude linefeeds
+	    		continue;
+	    	}
+	    	$filtered[$char]=$count;
+	    }
+	    
         // count every character on every line
-        $data = explode("\n", $data);
-        if (count($data) < 10) throw new Csv_Exception_DataSampleTooShort('You must provide at least ten lines in your sample data');
-        
-        $frequency = array();
+        $data = explode($linefeed, $data);
+        $tmp = array();
+        $linecount = 0;
         foreach ($data as $row) {
-            if (empty($row)) continue;
-            $frequency[] = count_chars($row, 1);
+            if (empty($row)) { 
+            	continue; 
+            }
+            
+            // count non-empty lines
+            $linecount++;
+            
+            // do a charcount on this line, but only remember the chars that
+            // survived the filtering above
+            $frequency = array_intersect_key(count_chars($row, 1), $filtered); 
+            
+            // store the charcount along with the previous counts
+	        foreach ($frequency as $char=>$count) {
+	        	if (!array_key_exists($char, $tmp)) {
+	        		$tmp[$char] = array();
+	        	}
+	        	$tmp[$char][] = $count; // this $char appears $count times on this line
+	        }
         }
         
-        // determine the "mode" for each character
-        $modes = array();
-        foreach ($frequency as $line) {
-            foreach ($line as $char => $count) {
-                //$ord = ord($char);
-                if (!isset($modes[$char]) || $count > $modes[$char])
-                    $modes[$char] = $count;
-            }
-        }
-        // count how many times each character matches its mode in a line
-        $temp = array();
-        foreach ($modes as $key => $mode) {
-            foreach ($frequency as $line) {
-                if (!isset($temp[chr($key)])) $temp[chr($key)] = 0;
-                if (isset($line[$key]) && $line[$key] == $mode) $temp[chr($key)]++;
-            }
+        // a potential delimiter must be present on every non-empty line 
+        foreach ($tmp as $char=>$array) {
+        	if (count($array)<0.98*$linecount) {
+        		// ... so drop any delimiters that aren't
+        		unset($tmp[$char]);
+        	}
         }
         
-        arsort($temp);
-        $times_matched = current($temp);
-        $lines = count($data);
-        $consistency = $times_matched / $lines;
-        $threshold = 0.9;
-        // if it is consistent enough, return the delimiter we think it is
-        $delim = key($temp);
-        if ($consistency > $threshold) return $delim;
-        return false;
+        foreach ($tmp as $char=>$array) {
+        	// a delimiter is very likely to occur the same amount of times on every line,
+        	// so drop delimiters that have too much variation in their frequency
+        	$dev = $this->deviation($array);
+        	if ($dev>0.5) { // threshold not scientifically determined or something
+        		unset($tmp[$char]);
+        		continue;
+        	}
+        	
+        	// calculate average number of appearances
+        	$tmp[$char] = array_sum($tmp[$char])/count($tmp[$char]);
+        }
+        
+        // now, prefer the delimiter with the highest average number of appearances
+        if (count($tmp)>0) {
+	        asort($tmp);
+	        $delim = chr(end(array_keys($tmp)));
+        } else {
+        	// no potential delimiters remain
+        	$delim = false;
+        }
+        
+        return $delim;
+    
+    }
+
+	protected function deviation ($array){
+    
+	    $avg = array_sum($array)/count($array);
+	    foreach ($array as $value) {
+	        $variance[] = pow($value-$avg, 2);
+	    }
+	    $deviation = sqrt(array_sum($variance)/count($variance));
+	    return $deviation;
+	
+    }    
+
+    protected function guessLinefeed($data) {
+    
+    	$charcount = count_chars($data);
+    	$cr = "\r";
+    	$lf = "\n";
+    	
+    	$count_cr = $charcount[ord($cr)];
+    	$count_lf = $charcount[ord($lf)];
+        
+    	if ($count_cr==$count_lf) {
+    		return "$cr$lf";
+    	}
+    	if ($count_cr==0 && $count_lf>0) {
+    		return "$lf";
+    	}
+    	if ($count_lf==0 && $count_cr>0) {
+    		return "$cr";
+    	}
+        
+    	// sane default: cr+lf
+    	return "$cr$lf";
+    
+    }
+    
+    protected function guessQuotingStyle($data, $quote, $delim, $linefeed) {
+    
+    	$dialect = new Csv_Dialect();
+    	$dialect->delimiter = $delim;
+    	$dialect->quotechar = $quote;
+    	$dialect->lineterminator = $linefeed;
+        
+    	$lines = explode($linefeed, $data);
+        
+    	$lines_processed = 0;
+    	
+        $reader = new Csv_Reader_String($data, $dialect);
+        $quotingstyle_count = array();
+        foreach ($reader as $parsedline) {
+        
+        	do {
+        		// fetch next line until a non-empty line is found
+        		$line = array_shift($lines);
+        	} while (strlen($line)==0);
+            
+        	// how much quotes are present in this line?
+        	$quote_count = substr_count($line, $quote);
+        	
+        	// how much quotes are in the data itself?
+        	$quotecount_in_data = substr_count(implode("", $parsedline), $quote);
+        	
+        	// how much columns are in this line?
+        	$column_count = count($parsedline);
+            
+			// default quoting style for this line: QUOTE_NONE
+        	$quotingstyle = Csv_Dialect::QUOTE_NONE;
+        	
+        	// determine this line's quoting style
+        	if ($quote_count==0 || $quote_count<=$quotecount_in_data) {
+        		// there are no quotes, or there are less quotes than the number of quotes in the data
+        		$quotingstyle = Csv_Dialect::QUOTE_NONE;
+        	} elseif ($quote_count>=($column_count*2)) {
+        		// the number of quotes is larger than, or equal to, the number of quotes 
+        		// necessary to quote each column 
+        		$quotingstyle = Csv_Dialect::QUOTE_ALL;
+        	} elseif ($quote_count>=$quotecount_in_data) {
+        		// there are more quotes than the number of quotes in the data
+        		$quotingstyle = Csv_Dialect::QUOTE_MINIMAL;
+        	}
+        	
+        	if (!array_key_exists($quotingstyle, $quotingstyle_count)) {
+        		$quotingstyle_count[$quotingstyle] = 0;
+        	}
+        	
+        	$quotingstyle_count[$quotingstyle]++;
+        	
+        	$lines_processed++;
+        	
+        	if ($lines_processed>15) {
+        		// don't process the whole file - stop processing after a while
+        		break;
+        	}
+        }
+        
+        // return the quoting style that was used most often
+        asort($quotingstyle_count);
+        $guess = end(array_keys($quotingstyle_count));
+        
+    	return $guess;
     
     }
 }
