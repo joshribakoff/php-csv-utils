a97b0a39d77bc239ccd385bc84b8d186a46aae29
luke.visinoni <luke.visinoni@0946aa64-6b46-0410-9bca-13cabbcf55d9>
2008-10-01 20:47:16 +0000

    turned tests back on (that I accidentally turned off on last commit)
    git-svn-id: http://php-csv-utils.googlecode.com/svn/trunk@48 0946aa64-6b46-0410-9bca-13cabbcf55d9

--- tests/index.php
+++ tests/index.php
@@ -64,8 +64,8 @@ require_once 'TestCases/Dialect.php';
 $test = new GroupTest('Core CSV Utilities Tests');
 $test->addTestCase(new Test_Of_Csv_Reader);
 $test->addTestCase(new Test_Of_Csv_Writer);
-//$test->addTestCase(new Test_Of_Csv_AutoDetect);
-//$test->addTestCase(new Test_Of_Csv_Dialect);
+$test->addTestCase(new Test_Of_Csv_AutoDetect);
+$test->addTestCase(new Test_Of_Csv_Dialect);
 if (TextReporter::inCli()) {
     exit ($test->run(new TextReporter()) ? 0 : 1);
 }
