270005c85b38131732efca939023999790fcc35b
luke.visinoni <luke.visinoni@0946aa64-6b46-0410-9bca-13cabbcf55d9>
2008-10-02 20:23:40 +0000

    fixed Csv_Reader_String - before it was reporting an incorrect amount of rows. now it's not a liar.
    git-svn-id: http://php-csv-utils.googlecode.com/svn/trunk@51 0946aa64-6b46-0410-9bca-13cabbcf55d9

--- Csv/Reader/String.php
+++ Csv/Reader/String.php
@@ -6,8 +6,14 @@ class Csv_Reader_String extends Csv_Reader {
      */
     public function __construct($string, Csv_Dialect $dialect = null) {
     
-        if (is_null($dialect)) $dialect = new Csv_Dialect;
+        if (is_null($dialect)) {
+            //$dialect = $this->autoDetectString($string);
+            $dialect = new Csv_Dialect;
+        }
         $this->dialect = $dialect;
+        // if last character isn't a line-break add one
+        $lastchar = substr($string, strlen($string)-1, 1);
+        if ($lastchar !== $dialect->lineterminator) $string = $string . $dialect->lineterminator;
         $this->handle = fopen("php://temp", 'w+');
         fwrite($this->handle, $string);
         unset($string);
