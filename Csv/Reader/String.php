10ba051be0ef3f382da3570f9ef0b435030ce03a
luke.visinoni <luke.visinoni@0946aa64-6b46-0410-9bca-13cabbcf55d9>
2008-10-03 00:19:00 +0000

    switched Csv_Reader_String to use memory instead of temp stream
    git-svn-id: http://php-csv-utils.googlecode.com/svn/trunk@57 0946aa64-6b46-0410-9bca-13cabbcf55d9

--- Csv/Reader/String.php
+++ Csv/Reader/String.php
@@ -16,7 +16,7 @@ class Csv_Reader_String extends Csv_Reader {
         // if last character isn't a line-break add one
         $lastchar = substr($string, strlen($string)-1, 1);
         if ($lastchar !== $dialect->lineterminator) $string = $string . $dialect->lineterminator;
-        $this->handle = fopen("php://temp", 'w+');
+        $this->handle = fopen("php://memory", 'w+');
         fwrite($this->handle, $string);
         unset($string);
         if ($this->handle === false) throw new Csv_Exception_FileNotFound('File does not exist or is not readable: "' . $path . '".');
