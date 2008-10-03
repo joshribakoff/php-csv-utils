cdf3ecdd4107985d6d638ccdc231c86be4d9df01
luke.visinoni <luke.visinoni@0946aa64-6b46-0410-9bca-13cabbcf55d9>
2008-10-03 00:07:46 +0000

    fixed the bug that was noted in the comments section of the blog - about Csv_Dialect_Excel using the literal '\r\n' instead of a newline
    git-svn-id: http://php-csv-utils.googlecode.com/svn/trunk@56 0946aa64-6b46-0410-9bca-13cabbcf55d9

--- Csv/Dialect/Excel.php
+++ Csv/Dialect/Excel.php
@@ -4,9 +4,9 @@ class Csv_Dialect_Excel extends Csv_Dialect
 {
     public $delimiter = ',';
     public $quotechar = '"';
-    public $escapechar = '\\';
+    public $escapechar = "\\";
     // public $doublequote = true;
     // public $skipinitialspace = false;
-    public $lineterminator = '\r\n';
+    public $lineterminator = "\r\n";
     public $quoting = Csv_Dialect::QUOTE_MINIMAL;
 }
