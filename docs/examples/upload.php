cdf3ddf2b0c7f205fc18963b7d887927cfe187f9
luke.visinoni <luke.visinoni@0946aa64-6b46-0410-9bca-13cabbcf55d9>
2008-10-03 00:41:35 +0000

    updating upload example - probably should just remove this and point to examples on my website instead
    git-svn-id: http://php-csv-utils.googlecode.com/svn/trunk@62 0946aa64-6b46-0410-9bca-13cabbcf55d9

--- docs/examples/upload.php
+++ docs/examples/upload.php
@@ -1,55 +1,47 @@
+<?php set_include_path(realpath('../../') . PATH_SEPARATOR . get_include_path()); ?>
 <html>
+<head>
+<title>Csv Uploader</title>
+</head>
+<body>
+<?php
 
-  <head>
-  
-    <title>Csv Uploader</title>
+if (!empty($_FILES['csv'])) {
 
-  </head>
-  
-  <body>
+require_once 'Csv/Exception/FileNotFound.php';
+require_once 'Csv/Reader.php';
+$filename = $_FILES['csv']['tmp_name'];
+try {
+echo "<table border='1'>";
+$reader = new Csv_Reader($filename);
+$row = $reader->getRow();
+echo "<tr>";
+foreach ($row as $header) printf("<th>%s</th>", $header);
+echo "</tr>";
+while ($row = $reader->getRow()) {
+echo "<tr>";
+foreach ($row as $col) printf("<td>%s</td>", $col);
+echo "</tr>";
+}
+echo "</table>";
+} catch (CSv_Exception_FileNotFound $e) {
+printf("<p>%s</p>", $e->getMessage());
+}
 
-  <?php
-  
-  if (!empty($_FILES['csv'])) {
-  
-      require_once '../../Csv/Dialect.php';
-      require_once '../../Csv/Exception.php';
-      require_once '../../Csv/Exception/FileNotFound.php';
-      require_once '../../Csv/Reader.php';
-      $filename = $_FILES['csv']['tmp_name'];
-      try {
-          echo "<table border='1'>";
-          $reader = new Csv_Reader($filename);
-          $row = $reader->getRow();
-          echo "<tr>";
-          foreach ($row as $header) printf("<th>%s</th>", $header);
-          echo "</tr>";
-          while ($row = $reader->getRow()) {
-              echo "<tr>";
-              foreach ($row as $col) printf("<td>%s</td>", $col);
-              echo "</tr>";
-          }
-          echo "</table>";
-      } catch (CSv_Exception_FileNotFound $e) {
-          printf("<p>%s</p>", $e->getMessage());
-      }
-  
-  } else {
-  
-  ?>
-  
-  <form method="post" action="./upload.php" enctype="multipart/form-data">
-  
-      <input type="file" name="csv"> <input type="submit">
-  
-  </form>
-  
-  <?php
-  
-  }
-  
-  ?>
-  
-  </body>
-  
+} else {
+
+?>
+
+<form method="post" action="./upload.php" enctype="multipart/form-data">
+<input type="file" name="csv"> <input type="submit">
+</form>
+
+<?php
+
+}
+
+?>
+
+</body>
+
 </html>
