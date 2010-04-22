049862247f38b0bb180224ba7c32fec568e1ef5f
luke.visinoni <luke.visinoni@0946aa64-6b46-0410-9bca-13cabbcf55d9>
2010-04-22 02:08:35 +0000

    Changed the name of the unit tests group back to what it was
    git-svn-id: http://php-csv-utils.googlecode.com/svn/trunk@80 0946aa64-6b46-0410-9bca-13cabbcf55d9

--- tests/index.php
+++ tests/index.php
@@ -63,8 +63,7 @@ require_once 'TestCases/Dialect.php';
 require_once 'TestCases/Docs.php';
 
 // run tests in html reporter
-// $test = new GroupTest('Core CSV Utilities Tests');
-$test = new GroupTest('Tests');
+$test = new GroupTest('Core CSV Utilities Tests');
 $test->addTestCase(new Test_Of_Csv_Reader);
 $test->addTestCase(new Test_Of_Csv_Writer);
 $test->addTestCase(new Test_Of_Csv_AutoDetect);
