de217340a2ec0d27317392297c7f01abbeee9b7a
josh <josh.ribakoff@gmail.com>
2013-05-22 21:37:06 -0400

    Updated .gitignore, updated tests/index.php for new version of simpletest

--- tests/index.php
+++ tests/index.php
@@ -12,7 +12,11 @@
 // set_include_path('/path/to/simpletest' . get_include_path());
 
 
-set_include_path(realpath('../') . PATH_SEPARATOR . get_include_path());
+set_include_path(
+    'simpletest/' . PATH_SEPARATOR .
+    realpath('../source/') . PATH_SEPARATOR .
+    get_include_path()
+);
 error_reporting(E_ALL & ~E_DEPRECATED);
 ini_set('display_errors', 1);
 
@@ -76,12 +80,12 @@ require_once 'TestCases/Dialect.php';
 require_once 'TestCases/Docs.php';
 
 // run tests in html reporter
-$test = new GroupTest('Core CSV Utilities Tests');
-$test->addTestCase(new Test_Of_Csv_Reader);
-$test->addTestCase(new Test_Of_Csv_Writer);
-$test->addTestCase(new Test_Of_Csv_AutoDetect);
-$test->addTestCase(new Test_Of_Csv_Dialect);
-$test->addTestCase(new Test_Of_Csv_Docs);
+$test = new TestSuite('Core CSV Utilities Tests');
+$test->add(new Test_Of_Csv_Reader);
+$test->add(new Test_Of_Csv_Writer);
+$test->add(new Test_Of_Csv_AutoDetect);
+$test->add(new Test_Of_Csv_Dialect);
+$test->add(new Test_Of_Csv_Docs);
 if (TextReporter::inCli()) {
     exit ($test->run(new TextReporter()) ? 0 : 1);
 }
